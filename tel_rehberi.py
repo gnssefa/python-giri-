a07bd21a582aa26b0f3f2b7145b177f08a67f311
Sefa <gnssefa0@gmail.com>
2025-11-05 21:27:52 +0300

    Ufak değişiklikler

--- tel_rehberi.py
+++ tel_rehberi.py
@@ -5,14 +5,14 @@ while True:
         take_name = input("Lütfen Kaydedilecek Kişinin İsmini Giriniz :")
         take_num = input("Lütfen Kişinin Numarasını Giriniz :")
 
-        if take_num.isdigit():
-            if len(take_num)==11:
-             pass
-            else :
-             print("Lütfen 11 Haneli numara girdiğinizden emin olunuz !!!")
-        else:
-             print("Lütfen sadece rakam giriniz")
-             break        
+        # if take_num.isdigit():
+        #     if len(take_num)==11:
+        #      pass
+        #     else :
+        #      print("Lütfen 11 Haneli numara girdiğinizden emin olunuz !!!")
+        # else:
+        #      print("Lütfen sadece rakam giriniz")
+        #      break        
 
         x = tel_rehberi.setdefault(take_name,take_num)
         print(f"{take_name} adlı kişi telefon rehberine eklendi")
